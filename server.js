a78bb047aa20e1ff910ce9d80e1cf590a3a5188e
Vijay <saivijaychandan@gmail.com>
2026-01-28 11:27:45 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const sendEmail = require('./utils/sendEmail');
+const app = express();
+
+app.use(express.json());
+
+app.post('/send', async (req, res) => {
+    const { to, subject, text, extraHtml } = req.body;
+
+    if (!to || !subject || !text) {
+        return res.status(400).json({ error: "Missing required fields (to, subject, text)" });
+    }
+
+    console.log(`Received request to email: ${to}`);
+
+    const success = await sendEmail(to, subject, text, extraHtml);
+
+    if (success) {
+        return res.status(200).json({ message: "Email sent successfully" });
+    } else {
+        return res.status(500).json({ error: "Failed to send email" });
+    }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Email Microservice running on port ${PORT}`));
